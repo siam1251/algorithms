1e892f77115a34343522cca079fc6cb35e07f4db
Sayem Mohammad Siam <sayemsiam@gmail.com>
2021-05-30 23:37:38 -0400

    Create KMP.cpp

---
+++ algorithm interview/KMP.cpp
@@ -0,0 +1,36 @@
+vector<int> build_pattern(){
+  vector<int> dp(token.size()+1, 0);
+  int j = 0;
+  for(int i = 1; i < token.size();){
+      if(token[i] == token[j]){
+          dp[i] = dp[j]+1;
+          j++; i++;
+      }else{
+
+          if(j != 0){
+             j = dp[j-1];
+          }else{
+              dp[i] = 0;
+              i++;
+          }
+      }
+
+  }
+  return dp;
+}
+
+bool KMP(string &s, string token){
+        
+    auto dp = build_pattern(token);
+    j = 0;
+    for(int i = 0; i < s.size();){
+        if(s[i] == token[j]){ j++;i++;}
+        else {
+            if(j != 0)
+                j = dp[j-1];
+            else i++;
+        }
+        if(j == token.size()) return true;
+    }
+    return false;
+}
